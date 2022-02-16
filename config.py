765a9fe60a93539a3cac60d379dbbee538440201
[TansyluAkh] <[TansyluAkh]>
2022-02-16 21:53:37 +0300

    new

--- config.py
+++ config.py
@@ -5,47 +5,59 @@ b2 = types.InlineKeyboardButton(text="⬜", callback_data="2")
 b3 = types.InlineKeyboardButton(text="⬜", callback_data="3")
 b4 = types.InlineKeyboardButton(text="⬜", callback_data="4")
 b5 = types.InlineKeyboardButton(text="⬜", callback_data="5")
-b6 = types.InlineKeyboardButton(text="enter", callback_data="16")
-b21 = types.InlineKeyboardButton(text="⬜", callback_data="21")
-b22 = types.InlineKeyboardButton(text="⬜", callback_data="22")
-b23 = types.InlineKeyboardButton(text="⬜", callback_data="23")
-b24 = types.InlineKeyboardButton(text="⬜", callback_data="24")
-b25 = types.InlineKeyboardButton(text="⬜", callback_data="25")
-b26 = types.InlineKeyboardButton(text="enter", callback_data="26")
-b31 = types.InlineKeyboardButton(text="⬜", callback_data="31")
-b32 = types.InlineKeyboardButton(text="⬜", callback_data="32")
-b33 = types.InlineKeyboardButton(text="⬜", callback_data="33")
-b34 = types.InlineKeyboardButton(text="⬜", callback_data="34")
-b35 = types.InlineKeyboardButton(text="⬜", callback_data="35")
-b36 = types.InlineKeyboardButton(text="enter", callback_data="36")
-b41 = types.InlineKeyboardButton(text="⬜", callback_data="41")
-b42 = types.InlineKeyboardButton(text="⬜", callback_data="42")
-b43 = types.InlineKeyboardButton(text="⬜", callback_data="43")
-b44 = types.InlineKeyboardButton(text="⬜", callback_data="44")
-b45 = types.InlineKeyboardButton(text="⬜", callback_data="45")
-b46 = types.InlineKeyboardButton(text="enter", callback_data="46")
-b51 = types.InlineKeyboardButton(text="⬜", callback_data="51")
-b52 = types.InlineKeyboardButton(text="⬜", callback_data="52")
-b53 = types.InlineKeyboardButton(text="⬜", callback_data="53")
-b54 = types.InlineKeyboardButton(text="⬜", callback_data="54")
-b55 = types.InlineKeyboardButton(text="⬜", callback_data="55")
-b56 = types.InlineKeyboardButton(text="enter", callback_data="56")
+b6 = types.InlineKeyboardButton(text="✅", callback_data="16")
+letters = ["⬜", "⬜","⬜","⬜","⬜","✅" ]
+today = 'гаилә'
+rows = 1
 word = ''
+open = False
 pos = 0
 gamekey = ''
-btns = [b1, b2, b3, b4, b5, b6, b21, b22, b23, b24, b25, b26, b31, b32, b33, b34, b35, b36, b41, b42, b43, b44, b45, b46, b51, b52,
-            b53, b54, b55, b56]
+btns = [b1, b2, b3, b4, b5, b6,]
 def array(change='', txt='', callback=''):
+    print('change:', change, 'txt:', txt, 'callb:', callback)
     keyboard = types.InlineKeyboardMarkup(row_width=6)
     if change != '':
-        btns[change] = types.InlineKeyboardButton(text=txt, callback_data=callback)
-    keyboard.add(*btns)
+        ind = change - (change // 6) * 6
+        btns[change] = types.InlineKeyboardButton(text=txt, callback_data=str(callback))
+        print(ind)
+        if ind!=4:
+            if letters[ind+1] == "⬜":
+                btns[change+1] = types.InlineKeyboardButton(text='_', callback_data=str(callback+1))
+        letters[ind] = txt
+    keyboard.row(*btns)
     return(keyboard, btns)
+
+
+def showres(linenum, res, correct):
+    print(linenum, 'line:;')
+    keyboard = types.InlineKeyboardMarkup(row_width=6)
+    start = (linenum-1)*6
+    for i in range(start, start+5):
+        print(i)
+        btns[i] = types.InlineKeyboardButton(text=res[i-start], callback_data='x')
+    if linenum != 5:
+        ls = ["⬜", "⬜", "⬜", "⬜", "⬜", "✅"]
+        for j in range(5):
+            if str(j) in correct.keys():
+                ls[j] = correct[str(j)]
+                btns.append(types.InlineKeyboardButton(text=correct[str(j)], callback_data=str(linenum+1)+str(j+1)))
+            else:
+                btns.append(types.InlineKeyboardButton(text="⬜", callback_data=str(linenum + 1) + str(j + 1)))
+        btns.append(types.InlineKeyboardButton(text="✅", callback_data=str(linenum+1)+"6"))
+    keyboard.add(*btns)
+    return(keyboard, btns, ls)
+
 def alifba():
-    keyboard1 = types.InlineKeyboardMarkup(row_width=8)
+    keyboard1 = types.InlineKeyboardMarkup(row_width=6)
     s = []
+    counter = 0
+    keyboard1.add(*btns)
     for i in alifbas:
+        counter += 1
         s.append(types.InlineKeyboardButton(text=i, callback_data=i))
-    keyboard1.add(*gamekey)
-    keyboard1.add(*s)
+        if counter == 8:
+            keyboard1.row(*s)
+            counter = 0
+            s = []
     return keyboard1
--- main.py
+++ main.py
@@ -4,43 +4,67 @@ initmarkup =  config.array(change='')[0]
 def convert(txt):
     if len(txt) >1:
         i, x = int(txt[0]), int(txt[1])
-        return(4*(i-1) +x)
+        return(6*(i-1) +(x-1))
     else:
         return(int(txt)-1)
 @bot.message_handler(commands=['start'])
 def start(m):
-    name = m.from_user.first_name
-    bot.send_message(m.chat.id, 'kelimelle', reply_markup = initmarkup)
-    bot.send_message(m.chat.id, 'Enter the letters')
-    config.gamekey = config.btns
+    bot.send_message(m.chat.id, 'Нажмите на серую ячейку, чтобы ввести букву', reply_markup = initmarkup)
+    config.gamekey = [config.b1, config.b2, config.b3, config.b4, config.b5, config.b6]
 
 @bot.message_handler(content_types=['text'])
 def save(m):
     config.word = m.text
     return
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.message:
         print(call.message)
         print(call.data, call.message.chat.id,' call.data')
         if call.data.isdigit():
-            if int(call.data) in [1, 2, 3, 4, 5, 21, 22, 23, 24, 25, 31, 32, 33, 34, 35,41, 42, 43, 44, 45, 51, 52, 53, 54, 55,]:
+            if int(call.data) in [2, 3, 4, 5,  22, 23, 24, 25, 32, 33, 34, 35,42, 43, 44, 45, 52, 53, 54, 55,1,21, 31, 41, 51]:
+                if config.open == False:
+                    config.pos = convert(call.data)
+                    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id,
+                                                  reply_markup=config.alifba())
+                    config.open = True
                 config.pos = convert(call.data)
-                bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=config.alifba())
-                print('o')
-            elif int(call.data) in [16, 26, 36, 46, 56]:
-                btn = (int(call.data[0])-1)*6
-                print(config.word)
-                arr = [i for i in config.word]
-                newkey = config.array(btn, arr, call.data)
-                bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup = newkey)
-                print('k')
-        else:
-            if call.data in config.alifbas:
-                newkey = config.array(config.pos, call.data, 'f')
-                config.gamekey = newkey[1]
-                bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=config.alifba())
 
+            elif call.data in ['16', '26', '36', '46', '56']:
+                res, correct = [], {}
+                flag = True
+                for l in range(5):
+                    if config.letters[l].lower() == config.today[l]:
+                       res.append(config.letters[l]+'🟩')
+                       correct[str(l)] = config.letters[l]
+                    else:
+                        flag = False
+                        if config.letters[l].lower() in config.today:
+                            res.append(config.letters[l]+'🟨')
+                        else:
+                            res.append(config.letters[l]+'⬛')
+                if flag:
+                    bot.send_message(call.message.chat.id, 'Вы отгадали слово дня: '+'\n'+config.today)
+                else:
+                    config.open = False
+                    config.pos = convert(str(int(call.data[0])+1)+'1')
+                    print(config.pos, 'POS')
+                    config.rows += 1
+                    reset = config.showres(int(call.data[0]), res, correct)
+                    config.gamekey = reset[1]
+                    config.letters = reset[2]
+                    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id,
+                                                  reply_markup=reset[0])
+                    print(config.letters)
+        if call.data in config.alifbas:
+            newkey = config.array(config.pos, call.data, config.pos+1)
+            config.gamekey = newkey[1]
+            ind = config.pos - (config.pos // 6) * 6
+            if ind!=4:
+                config.pos += 1
+            bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=config.alifba())
 
         return
 
